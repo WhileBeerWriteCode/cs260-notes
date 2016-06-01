d519494564170cab05ca2e08a0310809fb54a904
Joshua Harshman <joshgreyhat@gmail.com>
2016-05-31 21:01:41 -0700

    master
    add example code for taskswitcher

--- example-code/harshmanjhw4.asm
+++ example-code/harshmanjhw4.asm
@@ -1,7 +1,7 @@
 .data
 
-  tcb0: .word 32
-  tcb1: .word 32
+  tcb0: .space 128
+  tcb1: .space 128
   tid: .word 1
 
   taskzero_say: .asciiz "Hi I'm Task Zero!\n"
@@ -33,32 +33,164 @@
 
     savezero:
       la $t0, tcb0                      # load address of tcb0 (savezero)
-      sw $ra, 0($t0)                    # store return address
       lw $a0, 0($sp)                    # grab address of tcb0 from stack
       addi $sp, $sp, 4                  # restore stack
-      sw $a0, 8($t0)                    # store in tcb0 offset 8
+      #sw $a0, 8($t0)                    # store in tcb0 offset 8
 
       #TODO: Store other registers
+      # sw $2, 4($t0)
+      # sw $3, 8($t0)
+      # sw $4, 12($t0)
+      # sw $5, 16($t0)
+      # sw $6, 20($t0)
+      # sw $7, 24($t0)
+      # #sw $8, 28($t0)
+      # sw $9, 32($t0)
+      # sw $10, 36($t0)
+      # sw $11, 40($t0)
+      # sw $12, 44($t0)
+      # sw $13, 48($t0)
+      # sw $14, 52($t0)
+      # sw $15, 56($t0)
+      # sw $16, 60($t0)
+      # sw $17, 64($t0)
+      # sw $18, 68($t0)
+      # sw $19, 72($t0)
+      # sw $20, 76($t0)
+      # sw $21, 80($t0)
+      # sw $22, 84($t0)
+      # sw $23, 88($t0)
+      # sw $24, 92($t0)
+      # sw $25, 96($t0)
+      # sw $26, 100($t0)
+      # sw $27, 104($t0)
+      # sw $28, 108($t0)
+      # sw $29, 112($t0)
+      # sw $30, 116($t0)
+      sw $31, 120($t0)
+
+      lw $t1, 0($sp)
+      sw $t1, 28($t0)
 
       la $a1, tid                       # load tid
-      li $a1, 1                         # update tid to 1
-      la $t1, tcb1                      # load tcb1
-      lw $ra, 0($t1)                    # load return address
+      li $a2, 1                         # store 1 in register a2
+      sw $a2, ($a1)                     # update tid to 1
+      la $t0, tcb1                      # load tcb1
+
+      # TODO: Restore registers
+      # lw $2, 4($t0)
+      # lw $3, 8($t0)
+      # lw $4, 12($t0)
+      # lw $5, 16($t0)
+      # lw $6, 20($t0)
+      # lw $7, 24($t0)
+      # #lw $8, 28($t0)
+      # lw $9, 32($t0)
+      # lw $10, 36($t0)
+      # lw $11, 40($t0)
+      # lw $12, 44($t0)
+      # lw $13, 48($t0)
+      # lw $14, 52($t0)
+      # lw $15, 56($t0)
+      # lw $16, 60($t0)
+      # lw $17, 64($t0)
+      # lw $18, 68($t0)
+      # lw $19, 72($t0)
+      # lw $20, 76($t0)
+      # lw $21, 80($t0)
+      # lw $22, 84($t0)
+      # lw $23, 88($t0)
+      # lw $24, 92($t0)
+      # lw $25, 96($t0)
+      # lw $26, 100($t0)
+      # lw $27, 104($t0)
+      # lw $28, 108($t0)
+      # lw $29, 112($t0)
+      # lw $30, 116($t0)
+      lw $31, 120($t0)
+
+      lw $t0, 28($t0)                   # restore register t0
       jr $ra                            # return to task 1
 
     saveone:
-      la $t1, tcb1                      # load address of tcb1 (saveone)
-      sw $ra, 0($t1)                    # store return address
-      lw $a0, 0($sp)                    # grab address of tcb0 from stack
+      la $t0, tcb1                      # load address of tcb1 (saveone)
+      lw $a0, 0($sp)                    # grab address of tcb1 from stack
       addi $sp, $sp, 4                  # restore stack
-      sw $a0, 8($t1)                    # store in tcb0 offset 8
+      #sw $a0, 8($t1)                    # store in tcb1 offset 8
 
       #TODO: Store other registers
+      # sw $2, 4($t0)
+      # sw $3, 8($t0)
+      # sw $4, 12($t0)
+      # sw $5, 16($t0)
+      # sw $6, 20($t0)
+      # sw $7, 24($t0)
+      # #sw $8, 28($t0)
+      # sw $9, 32($t0)
+      # sw $10, 36($t0)
+      # sw $11, 40($t0)
+      # sw $12, 44($t0)
+      # sw $13, 48($t0)
+      # sw $14, 52($t0)
+      # sw $15, 56($t0)
+      # sw $16, 60($t0)
+      # sw $17, 64($t0)
+      # sw $18, 68($t0)
+      # sw $19, 72($t0)
+      # sw $20, 76($t0)
+      # sw $21, 80($t0)
+      # sw $22, 84($t0)
+      # sw $23, 88($t0)
+      # sw $24, 92($t0)
+      # sw $25, 96($t0)
+      # sw $26, 100($t0)
+      # sw $27, 104($t0)
+      # sw $28, 108($t0)
+      # sw $29, 112($t0)
+      # sw $30, 116($t0)
+      sw $31, 120($t0)
+
+      lw $t1, 0($sp)
+      sw $t1, 28($t0)
 
       la $a1, tid                       # load tid
-      li $a1, 0                         # update tid to 0
-      la $t1, tcb0                      # load tcb0
-      lw $ra, 0($t1)                    # load return address
+      li $a2, 0                         # store 0 in register a2
+      sw $a2, ($a1)                     # update tid to 0
+      la $t0, tcb0                      # load tcb0
+
+      # TODO: Restore registers
+      # lw $2, 4($t0)
+      # lw $3, 8($t0)
+      # lw $4, 12($t0)
+      # lw $5, 16($t0)
+      # lw $6, 20($t0)
+      # lw $7, 24($t0)
+      # #lw $8, 28($t0)
+      # lw $9, 32($t0)
+      # lw $10, 36($t0)
+      # lw $11, 40($t0)
+      # lw $12, 44($t0)
+      # lw $13, 48($t0)
+      # lw $14, 52($t0)
+      # lw $15, 56($t0)
+      # lw $16, 60($t0)
+      # lw $17, 64($t0)
+      # lw $18, 68($t0)
+      # lw $19, 72($t0)
+      # lw $20, 76($t0)
+      # lw $21, 80($t0)
+      # lw $22, 84($t0)
+      # lw $23, 88($t0)
+      # lw $24, 92($t0)
+      # lw $25, 96($t0)
+      # lw $26, 100($t0)
+      # lw $27, 104($t0)
+      # lw $28, 108($t0)
+      # lw $29, 112($t0)
+      # lw $30, 116($t0)
+      lw $31, 120($t0)
+
+      lw $t0, 28($t0)                   # restore register t0
       jr $ra                            # return to task 0
 
   __TASK_0:
